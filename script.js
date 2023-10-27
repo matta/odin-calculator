729d00be992aa7aaaea2750a759c8062c2979572
Matt Armstrong <matt@rfc20.org>
2023-10-26 20:11:14 -0700

    Update the display when the user presses a digit

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-let operandA = '';
-let operandB = '';
-let result = '';
+let operands = new Array;
 
 const add = (a, b) => +a + +b;
 const subtract = (a, b) => +a - +b;
 const multiply = (a, b) => +a * +b;
 const divide = (a, b) => +a / +b;
 const operate = (operator, a, b) => operator(a, b);
+
+const updateDisplay = () => {
+    let content = "\u2400";
+    if (operands.length > 0) {
+        content = operands[operands.length - 1];
+    }
+    const display = document.querySelector('#display');
+    display.textContent = content;
+}
+
+const onDigitPress = (digit) => {
+    if (operands.length == 0) {
+        operands.push(digit);
+    } else if (operands[operands.length - 1].length < 10) {
+        operands[operands.length - 1] += digit;
+    }
+}
+
+{
+    document.querySelectorAll('.digit-button').forEach(button => {
+        button.addEventListener('click', () => {
+            onDigitPress(button.textContent);
+            updateDisplay();
+        })
+    })
+
+    updateDisplay();
+}
