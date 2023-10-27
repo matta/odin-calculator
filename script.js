4e992f4a53d63bcce7b5278aad5651aa4dc6cae0
Matt Armstrong <matt@rfc20.org>
2023-10-26 19:38:32 -0700

    Checkpoint Javascript fragments

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+let operandA = '';
+let operandB = '';
+let result = '';
+
 const add = (a, b) => +a + +b;
 const subtract = (a, b) => +a - +b;
 const multiply = (a, b) => +a * +b;
 const divide = (a, b) => +a / +b;
+const operate = (operator, a, b) => operator(a, b);
